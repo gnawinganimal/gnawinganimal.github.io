30eee7fdd2c12c4dcd3faf317647c7f3e157af9e
gnawinganimal <unrongeur@proton.me>
2023-04-13 17:32:35 -0700

    Change layout so home is outside main content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./resume.html"],
+  content: ["./index.html", "./resume.html", "./src/yosemite.png"],
   darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         'sans': ["Karla"]
+      },
+      backgroundImage: {
+        'yosemite': "url('/src/yosemite.png')"
       },
     },
   },
